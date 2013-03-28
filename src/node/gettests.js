40533b1bf52b91ba028a46bc0ee9a4a5a4bf2c27
Vid <vid_github@zooid.org>
2013-03-28 13:01:59 -0400

    use repo nodemw

--- src/node/gettests.js
+++ src/node/gettests.js
@@ -1,4 +1,4 @@
-var bot = require('nodemw').bot;
+var bot = require('nodemw');
 var fs = require('fs');
 var utils = require( "./lib/util.js");
 
@@ -7,6 +7,7 @@ var wiki = getWiki();
 function getWiki() {
   require("../../"+utils.confFile);
   var wikiConfig = GLOBAL.CONFIG.wikiConfig;
+console.log(wikiConfig);
 
   var wikibot = new bot(wikiConfig);
 
@@ -16,41 +17,34 @@ function getWiki() {
 }
 
 function processTests(wikibot) {
-/*
+
   var params = {
     action: 'ask',
     query: '[[Test item type::+]]|?Test item page|?Test item type|?Test item description|?Test item content|?Test item tags|sort=Test item type'
   };
 
 	wikibot.api.call(params, function(info, next, data) {
- 		console.log("call:", data && data.query && data.query.results);
+processTestItems(data);
   });
-*/
 
-  wikibot.getAsk({ query: "[[Test item type::+]]|?Test item page|?Test item type|?Test item description|?Test item content|?Test item tags|sort=Test item page|sort=Test item type"}, processTestItems);
+//  wikibot.getAsk({ query: "[[Test item type::+]]|?Test item page|?Test item type|?Test item description|?Test item content|?Test item tags|sort=Test item page|sort=Test item type"}, processTestItems);
 }
 
 function processTestItems(data) {
+console.log(data);
   var features = {};
+debugger;
 
 var i = 0;
-  for (var r in data.info.results) {
+  for (var r in data.query.results) {
 //    var b = data.info.results[r]['printouts'];
-    var b = data.info.results[r]['printouts'];
-console.log(b);
-console.log("here", i++);
-console.log(b['Test item page']);
+    var b = data.query.results[r]['printouts'];
     var page = b['Test item page'][0].fulltext;
-console.log("here", i++);
     var type = b['Test item type'][0].fulltext;
-console.log("here", i++);
     var desc = b['Test item description'][0].fulltext;
-console.log("here", i++);
     var content = b['Test item content'][0].split("\n");
-console.log("here", i++);
 
     var feature = features[page] || "";
-console.log('feature', feature);
     feature += type + ": " + desc + "\n  " + content.join("\n  ") + "\n\n";
     features[page] = feature;
   }
