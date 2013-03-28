77a72a40190d7a6d28ba7ea17b8bc737f49535b4
Vid <vid_github@zooid.org>
2013-03-28 16:12:28 -0400

    remove manual modules

--- manual_modules/nodemw/examples/config-DIST.js
+++
@@ -1,7 +0,0 @@
-{
-	"server": "en.wikipedia.org",
-	"path": "/w",
-	"debug": false,
-	"username": "foo",
-	"password": "bar"
-}
--- manual_modules/nodemw/examples/config.js
+++
@@ -1,6 +0,0 @@
-{
-	"server": "poznan.wikia.com",
-	"debug": false,
-	"username": "Pyrabot",
-	"password": "pyr4pyr4"
-}
