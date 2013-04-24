ad272f3e0616a38384d4c1fa562b9306727e9320
Vid <vid_github@zooid.org>
2013-04-24 09:21:09 -0400

    more details

--- src/node/util/linesToJson.js
+++ src/node/util/linesToJson.js
@@ -2,25 +2,34 @@ var lineReader = require('line-reader');
 
 var hosts = [];
 var filename = process.argv[2];
+var last_comment = 'undefined';
 
 lineReader.eachLine(filename, function(line, last) {
 	var host = {};
-	host.name_s = line.replace(/^#[^a-z]*/, '').replace(/#.*/, '');
-	host.lastUpdate_dt = new Date().toISOString();
-	host.comment_s = 'Auto-imported';
-
-	host.active_b = true;
-	host.active_dt = host.lastUpdate_dt;
+	var now = new Date().toISOString();
+	var name = line.replace(/^#[^a-z]*/, '').replace(/#.*/, '');
+	host.name_s = name;
+	host.lastUpdate_dt = now;
+	host.comment_s = 'Auto-imported ' + now;
 	
-	if (/^##/.test(line)) {
-		host.active_b = false;
-	} else if (/^#/.test(line)) {
-		host.offline_b = true;
-		host.active_b = false;
-	}
-	if (host.name_s) {
+	if (name && !line.match(/^# .*/)) {
+		
+		if (/^##/.test(line)) {
+			host.inactive_dt = now;
+		} else if (/^#/.test(line)) {
+			host.offline_b = true;
+			host.offline_dt = now;
+			host.comment_s = last_comment;
+		} else {
+			host.active_b = true;
+			host.active_dt = now;
+		}
+		
 		hosts.push(host);
+	} else {
+		last_comment = name;
 	}
+	
 	if (last) {
 		console.log(JSON.stringify(hosts, null, '  '));
 	}
