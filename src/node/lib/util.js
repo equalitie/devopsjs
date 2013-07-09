30bd999d7fa0d698b99c9640a8ea1c29b968d914
Vid <vid_github@zooid.org>
2013-07-09 12:16:26 -0400

    modularize notify

--- src/node/lib/util.js
+++ src/node/lib/util.js
@@ -1,6 +1,20 @@
 exports.getTick = function() {
         return { tickTime : new Date().getTime(), tickDate : new Date().toISOString() }
 }
+exports.config = function() {
+  var configBase;
+  if (process.env.DEVOPSCONFIG) {
+    configBase = process.env.DEVOPSCONFIG;
+  } else {
+    configBase = process.cwd() + '/config/';
+  }
+
+  try {
+    require(configBase + 'localConfig.js');
+  } catch (e) {
+    throw 'Could not require "' + configBase + '/localConfig.js" — define DEVOPSCONFIG or run this program from its parent directory.';
+  }
+}
 
 exports.confFile = "config/localConfig.js";
 
