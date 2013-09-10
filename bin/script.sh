19fcbe6f8349587c4b0f8d455daeab96f4ce8bfb
Vid <vid_github@zooid.org>
2013-09-10 10:51:44 -0400

    remote timing attempt

--- bin/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-node src/node/gettests.js configs/wikiConfig.json
-for i in tests/features/*.generated.feature; do 
-				cucumber.js "$i" -f json > work/output.json
-				cat work/output.json | node src/node/cukeToSolr.js
-done
-
