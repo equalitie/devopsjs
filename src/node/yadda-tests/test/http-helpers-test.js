e152732e06d15effe2ab23253848bb05ef4ee844
cormac <cormac@studiorua.ie>
2013-10-23 11:06:16 +0100

    added tests for header functions

--- src/node/yadda-tests/test/http-helpers-test.js
+++ src/node/yadda-tests/test/http-helpers-test.js
@@ -18,9 +18,31 @@ describe('Test http helper functions', function () {
       .then(function (results) {
         results.forEach(function(result){
           expect(result).to.be(true);
-        });
+        })
+      })
+      .done(function(){
         done();
       });
+  });
+  it('should retrieve headers', function(done) {
+    var hostname   = 'noun.equalit.ie';
+    httpHelper.headers.retrieveHeaders(hostname)
+      .then(function (header) {
+        expect(header).not.to.be(undefined);
+      })
+      .done(function () {
+        done();
+      });
+  });
+
+
+  it('should check a header to see if it came from the cache', function() {
+    var viaHeader   = 'http/1.1 hetzner8.deflect.ca (ApacheTrafficServer/3.2.5 [uScMsSf pSeN:t cCMi p sS])',
+        isNotFromCache = httpHelper.headers.isNotFromCache(viaHeader);
+    expect(isNotFromCache).to.be(true);
+    viaHeader   = 'http/1.1 hetzner8.deflect.ca (ApacheTrafficServer/3.2.5 [uScHsSf pSeN:t cCMi p sS])',
+    isFromCache = httpHelper.headers.isFromCache(viaHeader);
+    expect(isFromCache).to.be(true);
   });
 
 });
