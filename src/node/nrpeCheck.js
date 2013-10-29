967678dc0f7ab82ace787cad333e44eb6440d703
David H. Mason <vid@floaty.(none)>
2013-10-29 16:37:05 -0400

    cleanup and merge dnets

--- src/node/nrpeCheck.js
+++ src/node/nrpeCheck.js
@@ -28,7 +28,14 @@ program
   .option('-s --save', 'write results (defaults to no if a check or host is specified')
   .parse(process.argv);
 
-hostLib = hostLib.setConfig(program, GLOBAL.CONFIG.configBase+ 'hosts.json');
+hostLib = hostLib.setConfig(program, 'MERGED');
+
+var hosts = [];
+GLOBAL.CONFIG.dnets.forEach(function(dnet) {
+  hosts = hosts.concat(hostLib.getDNET(dnet.replace(GLOBAL.CONFIG.domain, '')));
+});
+
+
 var doResolve = true; // resolve edge's current cluster
 var doSave = program.save || (!program.check && !program.host);
 var nrpeChecks = require('./lib/nrpe/allchecks.js').getChecks(program.check ? program.check : null);
@@ -37,8 +44,7 @@ for (var key in nrpeChecks) {
   if (nrpeChecks.hasOwnProperty(key)) { numChecks++; }
 }
 
-
-// only include one host
+// only include one host?
 if (program.host) {
   var onlyEdge = program.host;
   var newHosts = [];
@@ -74,20 +80,20 @@ if (doSave) {
   }
 }
 
-function processCheck(res) {
-      if (doSave) {
-        docs.push(res);
-        if (docs.length == (numChecks * hosts.length)) {
-          store.index({_index : 'devopsjs', _type : 'hostCheck', refresh : true}, docs);
-        }
-      } else {
-        console.log(res);
-      }
-    }
-
 for (var checkName in nrpeChecks) {
   for (var i = 0; i < hosts.length; i++) {
     var res = nrpe.checkHost(hosts[i].hostname, nrpeChecks[checkName], checkName, tick, processCheck) ;
+  }
+}
+
+function processCheck(res) {
+  if (doSave) {
+    docs.push(res);
+    if (docs.length == (numChecks * hosts.length)) {
+      store.index({_index : 'devopsjs', _type : 'hostCheck', refresh : true}, docs);
+    }
+  } else {
+    console.log(res);
   }
 }
 
@@ -113,7 +119,7 @@ function commitEdgeSummary(hosts, tick, store, nom) {
         host.dnetChange = tick.tickDate;
       }
     }
-    hostSummary.push({id: host.hostname + "/" + tick.tickTime, hostname: host.hostname, dnet: lookup, dnetChange : host.dnetChange, '@timestamp' : tick.tickDate, lonlat: host.lonlat, countryCode: host.countryCode});
+    hostSummary.push({hostname: host.hostname, dnet: lookup, dnetChange : host.dnetChange, '@timestamp' : tick.tickDate, lonlat: host.lonlat, countryCode: host.countryCode});
   }
   store.index({_index : 'devopsjs', _type : 'checkSummary'}, hostSummary);
   hostLib.writeHostsJson(hosts);
