cbae910a2d361ee1aed0cfd8c436cf811aa70b96
Vid <vid_github@zooid.org>
2013-10-29 19:05:58 -0400

    cleanup

--- src/node/util/mergedges.js
+++
@@ -1,7 +0,0 @@
-var staging = require('/home/devopsjs/n/devopsjs/config/hosts.staging.json');
-var deflect1 = require('/home/devopsjs/n/devopsjs/config/hosts.deflect1.json');
-
-var all = deflect1;
-all = all.concat(staging);
-
-console.log(JSON.stringify(all, null, '  '));
