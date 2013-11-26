6c4cb2c7a51746c44fea99c94a03926a94eb08f9
Vid <vid_github@zooid.org>
2013-11-26 15:31:07 -0500

    move to describe style

--- test/nomination.js
+++ test/nomination.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
-var nom = require('../src/node/lib/nomination.js');
-var expect = require('chai').expect;
+(function() {
+  var nom = require('../src/node/lib/nomination.js');
+  var expect = require('chai').expect;
 
-var testConfig = require('../config/testConfig.js');
-var cached;
+  var testConfig = require('../config/testConfig.js');
+  var cached;
 
-suite("nomination tests", function() {
-  setup(function(done){
-    nom.resolve(['hetzner7.deflect.ca', 'chime1.deflect.ca'], ['edge.deflect.ca', 'staging.deflect.ca'], function(r) {
-      cached = r;
+  describe("nomination tests", function() {
+    before(function(done){
+      nom.resolve(['hetzner7.deflect.ca', 'chime1.deflect.ca'], ['edge.deflect.ca', 'staging.deflect.ca'], function(r) {
+        cached = r;
+        done();
+      });
+    });
+
+    it("resolves", function testGetWiki(done) {
+      expect(nom.getConfig('hetzner7.deflect.ca')).to.not.equal(null);
       done();
     });
   });
-
-  test("resolve", function testGetWiki(done) {
-    expect(nom.getConfig('hetzner7.deflect.ca')).to.be.notnull;
-    done();
-  });
-});
+})();
 
